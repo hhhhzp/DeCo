dd1bbe1d82df66719c4707f21598ddf334a4ee43
hhhhzp <523470679@qq.com>
2025-12-13 07:10:40 +0800

    1

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ export MASTER_PORT=28778
 export NNODES=4
 export NGPUS_PER_NODE=8
 NODE_RANK=${NODE_RANK:-0}
-python main.py fit -c ./configs_c2i/DeCo_XL.yaml --trainer.num_nodes=4
+python main.py fit -c configs_c2i/ReCo_large.yaml --trainer.num_nodes=4
