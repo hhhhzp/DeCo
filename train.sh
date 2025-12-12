814d7ff69474791aa9d59922f5e9c46d9ac4368f
hhhhzp <523470679@qq.com>
2025-12-13 07:26:41 +0800

    1

--- train.sh
+++ train.sh
@@ -1,3 +1,15 @@
+export NCCL_IB_DISABLE=1
+export NCCL_IB_TIMEOUT=3600 
+export TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC=3600 
+export NCCL_LAUNCH_MODE=PARALLEL 
+export NCCL_IB_HCA=mlx5 
+export NCCL_IB_TC=136 
+export NCCL_IB_SL=5 
+export NCCL_IB_GID_INDEX=3 
+export NCCL_SOCKET_IFNAME=bond1
+# export NCCL_DEBUG=INFO
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+
 # multi-node training in lightning style, e.g., 4 nodes
 export MASTER_ADDR=29.111.44.218
 export MASTER_PORT=28778
