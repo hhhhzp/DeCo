be0f1bc263eeb4c056a20d5c8282a716d6506a69
hhhhzp <523470679@qq.com>
2025-12-13 06:42:04 +0800

    1

---
+++ train.sh
@@ -0,0 +1,7 @@
+# multi-node training in lightning style, e.g., 4 nodes
+export MASTER_ADDR={Your Config}
+export MASTER_PORT=28778
+export NNODES=4
+export NGPUS_PER_NODE=8
+NODE_RANK=${NODE_RANK:-0}
+python main.py fit -c ./configs_c2i/DeCo_XL.yaml --trainer.num_nodes=4
