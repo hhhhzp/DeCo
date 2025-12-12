aa8fa5942f5c19433578b787a7bd152260ec0b58
hhhhzp <523470679@qq.com>
2025-12-13 07:18:50 +0800

    1

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ export MASTER_ADDR=29.111.44.218
 export MASTER_PORT=28778
 export NNODES=4
 export NGPUS_PER_NODE=8
-NODE_RANK=${NODE_RANK:-0}
+export NODE_RANK=${NODE_RANK:-0}
 python main.py fit -c configs_c2i/ReCo_large.yaml --trainer.num_nodes=4
