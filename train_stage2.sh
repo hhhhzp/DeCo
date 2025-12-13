9688af72953b1ce14a57891a70644f6e78dc01ba
hhhhzp <523470679@qq.com>
2025-12-13 22:48:30 +0800

    1

--- train_stage2.sh
+++ train_stage2.sh
@@ -16,11 +16,11 @@ export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 # multi-node training in lightning style, e.g., 4 nodes
 export MASTER_ADDR=29.111.44.218
 export MASTER_PORT=18778
-export NNODES=1
+export NNODES=4
 export NGPUS_PER_NODE=8
 export NODE_RANK=${NODE_RANK:-0}
 
 python main.py fit -c configs_c2i/ReCo_large_stage2.yaml \
-    --trainer.num_nodes=1 \
+    --trainer.num_nodes=4 \
     --trainer.devices=8 \
     --trainer.strategy=ddp
