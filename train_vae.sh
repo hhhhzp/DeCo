cdf7a18da268b88e3ed21934d1cf1a2e25ed8230
hhhhzp <523470679@qq.com>
2025-12-16 21:56:31 +0800

    1

--- train_vae.sh
+++ train_vae.sh
@@ -16,10 +16,10 @@ export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 # multi-node training in lightning style, e.g., 4 nodes
 export MASTER_ADDR=29.111.44.218
 export MASTER_PORT=28778
-export NNODES=3
+export NNODES=4
 export NGPUS_PER_NODE=8
 export NODE_RANK=${NODE_RANK:-0}
 
 python main.py fit -c configs/vae_training.yaml \
-    --trainer.num_nodes=3 \
+    --trainer.num_nodes=4 \
     --trainer.devices=8
