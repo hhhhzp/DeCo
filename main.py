9b5b12897f959b403672f67f720e177ff0532452
hhhhzp <523470679@qq.com>
2025-12-16 15:23:59 +0800

    1

--- main.py
+++ main.py
@@ -7,20 +7,34 @@ from src.utils.patch_bugs import *
 from lightning import Trainer, LightningModule
 
 from src.lightning_data import DataModule
-from src.lightning_model import LightningModel
-from lightning.pytorch.cli import LightningCLI, LightningArgumentParser, SaveConfigCallback
+from src.lightning_model_ae import LightningModel
+from lightning.pytorch.cli import (
+    LightningCLI,
+    LightningArgumentParser,
+    SaveConfigCallback,
+)
 
 import logging
+
 logger = logging.getLogger("lightning.pytorch")
 # log_path = os.path.join( f"log.txt")
 # logger.addHandler(logging.FileHandler(log_path))
 
+
 class ReWriteRootSaveConfigCallback(SaveConfigCallback):
-    def save_config(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
+    def save_config(
+        self, trainer: Trainer, pl_module: LightningModule, stage: str
+    ) -> None:
         stamp = time.strftime('%y%m%d%H%M')
-        file_path = os.path.join(trainer.default_root_dir, f"config-{stage}-{stamp}.yaml")
+        file_path = os.path.join(
+            trainer.default_root_dir, f"config-{stage}-{stamp}.yaml"
+        )
         self.parser.save(
-            self.config, file_path, skip_none=False, overwrite=self.overwrite, multifile=self.multifile
+            self.config,
+            file_path,
+            skip_none=False,
+            overwrite=self.overwrite,
+            multifile=self.multifile,
         )
 
 
@@ -35,14 +49,24 @@ class ReWriteRootDirCli(LightningCLI):
 
     def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
         class TagsClass:
-            def __init__(self, exp:str):
-                ...
+            def __init__(self, exp: str): ...
+
         parser.add_class_arguments(TagsClass, nested_key="tags")
 
     def add_default_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
         super().add_default_arguments_to_parser(parser)
-        parser.add_argument("--torch_hub_dir", type=str, default=None, help=("torch hub dir"),)
-        parser.add_argument("--huggingface_cache_dir", type=str, default=None, help=("huggingface hub dir"),)
+        parser.add_argument(
+            "--torch_hub_dir",
+            type=str,
+            default=None,
+            help=("torch hub dir"),
+        )
+        parser.add_argument(
+            "--huggingface_cache_dir",
+            type=str,
+            default=None,
+            help=("huggingface hub dir"),
+        )
 
     def instantiate_trainer(self, **kwargs: Any) -> Trainer:
         config_trainer = self._get(self.config_init, "trainer", default={})
@@ -57,7 +81,11 @@ class ReWriteRootDirCli(LightningCLI):
         default_root_dir = os.path.join(default_root_dir, dirname)
         is_resume = self._get(self.config_init, "ckpt_path", default=None)
         if os.path.exists(default_root_dir) and "debug" not in default_root_dir:
-            if os.listdir(default_root_dir) and self.subcommand != "predict" and not is_resume:
+            if (
+                os.listdir(default_root_dir)
+                and self.subcommand != "predict"
+                and not is_resume
+            ):
                 raise FileExistsError(f"{default_root_dir} already exists")
 
         config_trainer.default_root_dir = default_root_dir
@@ -76,9 +104,13 @@ class ReWriteRootDirCli(LightningCLI):
             torch.hub.set_dir(torch_hub_dir)
         super().instantiate_classes()
 
+
 if __name__ == "__main__":
 
-    cli = ReWriteRootDirCli(LightningModel, DataModule,
-                            auto_configure_optimizers=False,
-                            save_config_callback=ReWriteRootSaveConfigCallback,
-                            save_config_kwargs={"overwrite": True})
+    cli = ReWriteRootDirCli(
+        LightningModel,
+        DataModule,
+        auto_configure_optimizers=False,
+        save_config_callback=ReWriteRootSaveConfigCallback,
+        save_config_kwargs={"overwrite": True},
+    )
