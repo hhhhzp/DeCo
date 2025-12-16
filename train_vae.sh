3939c499d5632e13f593f0e0b7baa27b133d1a1a
hhhhzp <523470679@qq.com>
2025-12-17 00:01:50 +0800

    1

--- train_vae.sh
+++ train_vae.sh
@@ -22,5 +22,4 @@ export NODE_RANK=${NODE_RANK:-0}
 
 python main.py fit -c configs/vae_training.yaml \
     --trainer.num_nodes=4 \
-    --trainer.devices=8 \
-    --ckpt_path=vae_training_workdir/exp_VAE_Encoder_Training/last.ckpt
+    --trainer.devices=8
