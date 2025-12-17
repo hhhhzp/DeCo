fe00253db814f471fabf0b297ea0f45a55f5e6e7
hhhhzp <523470679@qq.com>
2025-12-17 20:49:46 +0800

    1

--- train_vae_stage2.sh
+++ train_vae_stage2.sh
@@ -23,4 +23,4 @@ export NODE_RANK=${NODE_RANK:-0}
 python main.py fit -c configs/vae_training_stage2.yaml \
     --trainer.num_nodes=4 \
     --trainer.devices=8 \
-    --ckpt_path=vae_stage2/exp_VAE_Encoder_Training/epoch=3-step=8000.ckpt
+    --ckpt_path=vae_stage2/exp_VAE_Encoder_Training/epoch=19-step=50000.ckpt
