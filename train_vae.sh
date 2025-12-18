12f7d06e09f9b4a9a6482f379a9a8997bcf7f64e
hhhhzp <523470679@qq.com>
2025-12-19 00:11:41 +0800

    1

--- train_vae.sh
+++ train_vae.sh
@@ -22,5 +22,4 @@ export NODE_RANK=${NODE_RANK:-0}
 
 python main.py fit -c configs/vae_training.yaml \
     --trainer.num_nodes=4 \
-    --trainer.devices=8 \
-    --ckpt_path=Sphere_vae_training_workdir/exp_Sphere_VAE_Stage1/epoch=0-step=14000.ckpt
+    --trainer.devices=8
