11f76b7e7fc75776d7c5c4a21c85f0b1897b9537
hhhhzp <523470679@qq.com>
2025-12-19 12:04:14 +0800

    1

--- src/lightning_model_ae.py
+++ src/lightning_model_ae.py
@@ -159,7 +159,7 @@ class LightningModelVAE(pl.LightningModule):
         # Compile models for efficiency AFTER loading pretrained weights
         self.vae_model = torch.compile(self.vae_model)
         self.ema_vae_model = torch.compile(self.ema_vae_model)
-        # self.loss_module = torch.compile(self.loss_module)
+        self.loss_module = torch.compile(self.loss_module)
 
     def _get_module(self, module):
         """Helper to unwrap DDP module if needed."""
