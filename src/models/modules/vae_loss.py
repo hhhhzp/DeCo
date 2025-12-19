59ad14fc6ff97b56077c35b5d59702b04c170d3d
hhhhzp <523470679@qq.com>
2025-12-19 09:47:24 +0800

    1

--- src/models/modules/vae_loss.py
+++ src/models/modules/vae_loss.py
@@ -215,24 +215,6 @@ class VAEReconstructionLoss(nn.Module):
         )
         vision_config = config.vision_config
         vision_config.drop_path_rate = 0.0
-
-        vit_hidden_size = config.vision_config.hidden_size
-        llm_hidden_size = config.llm_config.hidden_size
-
-        # Create teacher vision model
-        self.teacher_vision_model = InternVisionModel(vision_config)
-
-        # Create teacher mlp1
-        self.teacher_mlp1 = nn.Sequential(
-            nn.LayerNorm(vit_hidden_size * int(1 / self.downsample_ratio) ** 2),
-            nn.Linear(
-                vit_hidden_size * int(1 / self.downsample_ratio) ** 2,
-                llm_hidden_size,
-            ),
-            nn.GELU(),
-            nn.Linear(llm_hidden_size, llm_hidden_size),
-        )
-
         # Load pretrained weights
         model = AutoModel.from_pretrained(
             pretrained_model_path,
@@ -242,19 +224,10 @@ class VAEReconstructionLoss(nn.Module):
         )
 
         # Extract vision_model and mlp1 to teacher model
-        self.teacher_vision_model.load_state_dict(model.vision_model.state_dict())
-        self.teacher_mlp1.load_state_dict(model.mlp1.state_dict())
-
-        # Freeze entire teacher model
-        for param in self.teacher_vision_model.parameters():
-            param.requires_grad = False
-        for param in self.teacher_mlp1.parameters():
-            param.requires_grad = False
-
-        # Set to eval mode
-        self.teacher_vision_model.eval()
-        self.teacher_mlp1.eval()
-
+        self.teacher_vision_model = model.vision_model
+        self.teacher_mlp1 = model.mlp1
+        no_grad(self.teacher_vision_model)
+        no_grad(self.teacher_mlp1)
         print("Teacher model loaded and frozen successfully!")
 
     def pixel_shuffle(self, x, scale_factor=0.5):
