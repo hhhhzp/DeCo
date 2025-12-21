5cecaef5a29632a3d7ef5716ce7214119911a3ec
hhhhzp <523470679@qq.com>
2025-12-21 11:05:47 +0800

    1

--- src/lightning_model_ae.py
+++ src/lightning_model_ae.py
@@ -206,12 +206,11 @@ class LightningModelVAE(pl.LightningModule):
             param_groups.append({"params": other_params, "lr_scale": 1.0})
 
         optimizer_encoder = self.optimizer(param_groups)
-        lr_scheduler_encoder = get_constant_schedule_with_warmup(
-            optimizer_encoder, num_warmup_steps=0
+        lr_scheduler_encoder = get_cosine_schedule_with_warmup(
+            optimizer_encoder, num_warmup_steps=10000, num_training_steps=200000
         )
-        # get_cosine_schedule_with_warmup(
-        #     optimizer_encoder, num_warmup_steps=10000, num_training_steps=200000
-        # )
+
+        # get_constant_schedule_with_warmup(optimizer_encoder, num_warmup_steps=0)
 
         # Optimizer for discriminator
         loss_module = self._get_module(self.loss_module)
