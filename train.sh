225c576b2cfa4ee2f4ba2fb2abe702c6ffff9c8f
hhhhzp <523470679@qq.com>
2026-01-07 21:39:21 +0800

    1

--- train.sh
+++ train.sh
@@ -23,13 +23,13 @@ export NGPUS_PER_NODE=8
 export NODE_RANK=${NODE_RANK:-0}
 
 echo "=== Starting experiments from layer 24 to layer 6 ==="
-for layer in 2 4 8 12; do
+for layer in 4 8 12 16; do
     echo "Running experiment for layer ${layer}..."
     python main.py fit -c configs_flow/internvit_2b_base.yaml \
         --model.denoiser.init_args.select_layer=${layer} \
         --tags.exp=internvit_1024d_layer_${layer} \
         --trainer.logger.init_args.name=internvit_1024d_layer_${layer} \
-        --trainer.max_steps 10100 \
+        --trainer.max_steps 5100 \
         --trainer.num_nodes=4 \
         --trainer.devices=8 \
         --trainer.strategy=ddp
