38dd217c15731ff3abd1a259245f7f38d8689b83
hhhhzp <523470679@qq.com>
2026-01-11 22:33:40 +0800

    1

--- src/models/uniflow/modeling_uniflow.py
+++ src/models/uniflow/modeling_uniflow.py
@@ -1350,7 +1350,7 @@ class UniFlowVisionModel(PreTrainedModel):
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=0.02)
+            torch.nn.init.xavier_uniform_(m.weight)
             if isinstance(m, nn.Linear) and m.bias is not None:
                 nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.LayerNorm):
