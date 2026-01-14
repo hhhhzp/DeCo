cdc3756b1e218e0b75f24a9b3a118aa4ed3cb317
hhhhzp <523470679@qq.com>
2026-01-14 15:17:34 +0800

    1

--- load_vlm.py
+++ load_vlm.py
@@ -6,3 +6,10 @@ model = AutoModel.from_pretrained(
     dtype=torch.bfloat16,
     trust_remote_code=True,
 )
+
+s = torch.load(
+    "dual_internvit_2b/exp_sem_ae_mlp_c128_cosine/epoch=29-step=100000.ckpt",
+    map_location='cpu',
+)
+msg = model.vision_model.load(s['state_dict'])
+print(msg)
