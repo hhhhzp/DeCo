7cf9df106216a9425fe94caee55e78d356350036
hhhhzp <523470679@qq.com>
2026-01-15 01:09:51 +0800

    1

--- train_uni_448px.sh
+++ train_uni_448px.sh
@@ -24,5 +24,5 @@ export NODE_RANK=${NODE_RANK:-0}
 python main.py fit -c  configs_flow/uniflow_internvit_2b_blip3o_448px.yaml \
     --trainer.num_nodes=4 \
     --trainer.devices=8 \
-    --ckpt_path=dual_internvit_2b/exp_layer6_r14_mlp_c32/epoch=7-step=25000.ckpt
+    --ckpt_path=dual_internvit_2b/exp_sem_layer6_r14_mlp_c32/epoch=7-step=25000.ckpt
 
