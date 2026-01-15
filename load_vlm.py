a89c3f667f8c24d0fcf4d572c25a30eee9da525b
hhhhzp <523470679@qq.com>
2026-01-15 20:42:13 +0800

    1

--- load_vlm.py
+++ load_vlm.py
@@ -2,7 +2,7 @@ from transformers import AutoModel
 import torch
 
 model = AutoModel.from_pretrained(
-    "./InternVL3-2B-bak",
+    "./InternVL3-2B",
     dtype=torch.bfloat16,
     trust_remote_code=True,
 )
