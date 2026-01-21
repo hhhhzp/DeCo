7228f1c844e0e40ee6189f417e9a6132c8e114d3
hhhhzp <523470679@qq.com>
2026-01-21 23:51:09 +0800

    1

--- test_vlm.py
+++ test_vlm.py
@@ -18,7 +18,7 @@ import torchvision.transforms as transforms
 image = Image.open("examples/image1.jpg").convert("RGB")
 transform = transforms.Compose(
     [
-        transforms.Resize((448, 448)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
